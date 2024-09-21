54bf0bd339a93d72024ee73d4be4dbd806602f05
sahishnu111 <b523055@iiit-bh.ac.in>
2024-09-21 21:41:47 +0530

    added checklist and placeholder and ...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import {useState} from "react";
+
 const initialItems = [
     { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 3, description: "Underwear", quantity: 12, packed: false },
 ];
 
 export default function App() {
@@ -19,10 +22,34 @@ function Logo() {
 }
 
 function Form() {
+    const [description, setDescription] = useState("");
+    const[quantity,setQuantity] = useState(1);
+
+    function handleSubmit(e){
+    e.preventDefault();
+    if(!description.trim()) return;
+
+    const newItem={description,quantity,packed:false},id=initialItems.length+1
+        setDescription("")
+        setQuantity(1)
+    };
+    //initialItems.push(newItem);
+
+
     return (
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your trip ➕?</h3>
-        </div>
+            <select value={quantity}
+                    onChange={(e)=>setQuantity(Number(e.target.value))}>
+                {Array.from({length:20},(_,i)=>i+1).map((num)=>
+                    <option value={num} key={num}>
+                    {num}
+                </option>)}
+            </select>
+            <input type="text" placeholder="Item" value={description}
+                   onChange={(e)=>setDescription(e.target.value)}/>
+            <button onClick={handleSubmit}>Add</button>
+        </form>
     );
 }
 
@@ -42,7 +69,10 @@ function PackingList() {
 function Item({ item }) {
     return (
         <li>
-            {item.description} - Quantity: {item.quantity}
+            <span style={item.packed?{textDecoration:"line-through"}:{}}>
+                {item.description} - Quantity: {item.quantity}
+            </span>
+            <button>{item.packed?"✔️":"❌"}</button>
         </li>
     );
 }
